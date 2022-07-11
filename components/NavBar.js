a4390cfd86cf853928c1f2c9d62701451978e1bc
DADA YO <dayeon.yoon@gmail.com>
2022-07-11 22:34:17 +0900

    FIX: Link error

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function NavBar() {
+  const router = useRouter();
+
   return (
     <nav>
-      <a href="/">Home</a>
-      <a href="about">About</a>
+      <Link href={"/"}>
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+      </Link>
+      <Link href={"about"}>
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
+      </Link>
     </nav>
   );
 }

b611ee8598ec3b7c36c9485427af68ac5f4038f9
DADA YO <dayeon.yoon@gmail.com>
2022-07-14 01:31:57 +0900

    FEAT: Add movie detail url

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,32 @@ import Seo from "../components/Seo";
 
 export default function Home({ results }) {
   const router = useRouter();
-  const onClick = (id) => {
-    router.push(`/movies/${id}`);
+  const onClick = (id, title) => {
+    router.push(
+      { pathname: `/movies/${id}`, query: { title } },
+      `/movies/${id}`
+    );
   };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div onClick={() => onClick(movie.id)} className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+            <Link
+              href={
+                ({
+                  pathname: `/movies/${movie.id}`,
+                  query: { title: movie.original_title },
+                },
+                `/movies/${movie.id}`)
+              }
+            >
               <a>{movie.original_title}</a>
             </Link>
           </h4>
